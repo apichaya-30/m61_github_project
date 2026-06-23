c71a0d82a364a45c4a01d02b8be7aca935035c3c
apichaya-30 <164404@wsk.ac.th>
2026-06-23 15:08:41 +0700

    Update คำนวณค่าbmi.py

--- คำนวณค่าbmi.py
+++ คำนวณค่าbmi.py
@@ -1,1 +1,19 @@
+ โปรแกรมคำนวณค่า BMI พร้อมแปลผล
 
+weight = float(input("กรอกน้ำหนัก (กก.): "))
+height = float(input("กรอกส่วนสูง (เมตร): "))
+
+bmi = weight / (height ** 2)
+
+print("ค่า BMI =", round(bmi, 2))
+
+if bmi < 18.5:
+    print("อยู่ในเกณฑ์ผอม")
+elif bmi < 23:
+    print("อยู่ในเกณฑ์ปกติ")
+elif bmi < 25:
+    print("น้ำหนักเกิน")
+elif bmi < 30:
+    print("โรคอ้วนระดับ 1")
+else:
+    print("โรคอ้วนระดับ 2")
